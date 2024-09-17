9d28948ecd8e57b2061e699e024390dbecf4c2c5
omaima <benhlima.omaima.solicode@gmail.com>
2024-09-17 14:16:18 +0100

    last commit add padZero function

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -20,10 +20,10 @@ function DigitalClock() {
         let seconds = time.getSeconds();
         const meridiem = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12;
-        return `${hours}:${minutes}:${seconds} ${meridiem}`
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`
     }
-    function padZero() {
-        
+    function padZero(number) {
+        return (number < 10 ? "0" : "") + number;  
     }
     return (
         <div className="clock-container">
